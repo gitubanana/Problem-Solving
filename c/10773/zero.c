8382dc3f58cb135e7eac426a7bc49b266320d1ce
gitubanana <ptyolo666@gmail.com>
2022-06-17 10:04:44 +0900

    zero

---
+++ c/10773/zero.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	get_sum(int *arr, int size);
+
+int	main(void)
+{
+	int	n, tmp;
+	int	top = -1;
+	int	*stack;
+
+	scanf("%d", &n);
+	stack = calloc(n, sizeof(int));
+	while (n--)
+	{
+		scanf("%d", &tmp);
+		if (tmp == 0 && top > -1)
+			stack[top--] = 0;
+		else
+			stack[++top] = tmp;
+	}
+	printf("%d", get_sum(stack, top + 1));
+}
+
+int	get_sum(int *arr, int size)
+{
+	int	i, sum;
+
+	sum = 0;
+	for (i = 0; i < size; i++)
+		sum += arr[i];
+	return sum;
+}
