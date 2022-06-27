93c532e31e13ec7837d8dd583aa908b72075bda0
gitubanana <ptyolo666@gmail.com>
2022-06-27 18:50:21 +0900

    search_doc

---
+++ c/brute_force/1543/search_doc.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int	main(void)
+{
+	int	cnt, len;
+	char	*tmp;
+	char	aim[51], doc[2501];
+
+	scanf("%[^\n] %[^\n]", doc, aim);
+	tmp = doc;
+	len = strlen(aim);
+	while (tmp = strstr(tmp, aim))
+	{
+		cnt++;
+		tmp += len;
+	}
+	printf("%d", cnt);
+}
