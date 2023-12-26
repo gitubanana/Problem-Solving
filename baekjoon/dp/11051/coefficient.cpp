f8638f26251124e6559058e1b703e384fb3df04c
gitubanana <ptyolo666@gmail.com>
2023-12-26 20:44:06 +0900

    sovled coefficient using c++

---
+++ baekjoon/dp/11051/coefficient.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+const int   MAX = 1001;
+const int   DIVISOR = 10007;
+
+int dp[MAX][MAX];
+
+int main(void)
+{
+    std::cin.tie(0)->sync_with_stdio(0);
+
+    int N, R;
+
+    std::cin >> N >> R;
+    for (int n = 0; n <= N; ++n)
+    {
+        dp[n][0] = 1;
+        for (int r = 1; r <= R; ++r)
+        {
+            if (n == r)
+                dp[n][r] = 1;
+            else
+                dp[n][r] = ((dp[n - 1][r - 1] % DIVISOR)
+                            + (dp[n - 1][r] % DIVISOR)) % DIVISOR;
+        }
+    }
+
+    std::cout << dp[N][R] << '\n';
+    return (0);
+}
