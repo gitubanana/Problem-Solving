f931f6dfc962e06ce31ec5052a713880d57aa638
gitubanana <ptyolo666@gmail.com>
2024-05-17 10:30:57 +0900

    solved algorithm5 using c++

---
+++ baekjoon/implementation/24266/algorithm5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(void)
+{
+    std::cin.tie(0)->sync_with_stdio(0);
+
+    uint64_t n;
+
+    std::cin >> n;
+    std::cout << n * n * n << '\n';
+    std::cout << '3' << '\n';
+    return (0);
+}
