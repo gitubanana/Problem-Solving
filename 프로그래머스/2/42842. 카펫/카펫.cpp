cc34e4bb0cb357fef6241c627f1b04474bc8b675
Taeyang Park <101627885+gitubanana@users.noreply.github.com>
2024-08-28 16:49:30 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -6,28 +6,21 @@ using namespace std;
 vector<int> solution(int brown, int yellow) {
     vector<int> answer;
 
-    int ySize = 3;
-    while (true)
+    int totalCnt = brown + yellow;
+    for (int ySize = 3; ; ++ySize)
     {
-        int xSize = ySize;
-        while (true)
+        int xSize = totalCnt / ySize;
+        if (xSize * ySize == totalCnt)
         {
             int brownCnt = ySize * 2 + (xSize - 2) * 2;
-            if (brownCnt > brown)
-                break ;
 
-            int yellowCnt = ySize * xSize - brownCnt;
-            if (brown == brownCnt && yellow == yellowCnt)
+            if (brownCnt == brown)
             {
                 answer.push_back(xSize);
                 answer.push_back(ySize);
                 return (answer);
             }
-
-            ++xSize;
-        }
-
-        ++ySize;
+        }        
     }
     return answer;
 }
