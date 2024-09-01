4d05fe5fb4c4b9b215e76e1eaee16498d9f352fb
Taeyang Park <101627885+gitubanana@users.noreply.github.com>
2024-09-01 16:13:48 +0900

    [level 3] Title: 길 찾기 게임, Time: 16.71 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.cpp
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.cpp
@@ -5,95 +5,89 @@
 
 using namespace std;
 
-enum e_idx
-{
-    LEFT = 0,
-    RIGHT
-};
-
 enum e_pos
 {
     X = 0,
     Y
 };
 
+enum e_idx
+{
+    PRE = 0,
+    POST
+};
+
 struct t_pos
 {
     int y, x;
     int num;
-
-    inline bool operator<(const t_pos &other)
-    {
-        if (this->y == other.y)
-            return (this->x > other.x);
-
-        return (this->y > other.y);
-    }
 };
 
 const int MAX_V = 1e4 + 1;
-const int ROOT = 0;
 
-int child[MAX_V][2];
 vector<vector<int>> answer(2, vector<int>());
 vector<t_pos> map;
 
-void    insertNode(int putIdx, int cmpIdx=ROOT)
+int getRootIdx(const vector<int> &tree)
 {
-    const t_pos &cmp = map[cmpIdx];
-    const t_pos &put = map[putIdx];
+    int rootIdx;
+    int maxY = -1;
 
-    int &nextCmp = child[cmpIdx][(cmp.x < put.x)];
-    if (nextCmp == 0)
+    for (const int &idx : tree)
     {
-        nextCmp = putIdx;
+        const int &cmpY = map[idx].y;
+
+        if (maxY < cmpY)
+        {
+            maxY = cmpY;
+            rootIdx = idx;
+        }
+    }
+
+    return (rootIdx);
+}
+
+void    makeTree(const vector<int> &tree)
+{
+    if (tree.empty())
         return ;
-    }
 
-    insertNode(putIdx, nextCmp);
-}
+    int rootIdx = getRootIdx(tree);
+    const t_pos &root = map[rootIdx];
+    vector<int> leftTree, rightTree;
 
-void    preorder(int cur, vector<int> &order)
-{
-    order.push_back(map[cur].num);
-    for (int i = LEFT; i <= RIGHT; ++i)
+    answer[PRE].push_back(root.num);
+    for (const int &idx : tree)
     {
-        const int &next = child[cur][i];
-        if (next == 0)
+        const t_pos &cmp = map[idx];
+        if (idx == rootIdx)
             continue ;
 
-        preorder(next, order);
+        if (root.x < cmp.x)
+        {
+            rightTree.push_back(idx);
+        }
+        else
+        {
+            leftTree.push_back(idx);
+        }
     }
-}
-
-void    postorder(int cur, vector<int> &order)
-{
-    for (int i = LEFT; i <= RIGHT; ++i)
-    {
-        const int &next = child[cur][i];
-        if (next == 0)
-            continue ;
-
-        postorder(next, order);
-    }
-    order.push_back(map[cur].num);
+    makeTree(leftTree);
+    makeTree(rightTree);
+    answer[POST].push_back(root.num);
 }
 
 vector<vector<int>> solution(vector<vector<int>> nodeinfo)
 {
-    int num = 1;
-    for (const vector<int> &vec : nodeinfo)
-    {
-        map.push_back({vec[Y], vec[X], num++});
-    }
-    std::sort(map.begin(), map.end());
+    int num = 0;
+    vector<int> tree;
 
-    for (int i = 1; i < map.size(); ++i)
+    for (const vector<int> &node : nodeinfo)
     {
-        insertNode(i);
+        tree.push_back(num++);
+        map.push_back({node[Y], node[X], num});
     }
 
-    preorder(ROOT, answer[0]);
-    postorder(ROOT, answer[1]);
+    makeTree(tree);
     return answer;
 }
