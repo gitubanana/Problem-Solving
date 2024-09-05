e249dda10accf5bbab0b7f039e2fd049e093b5f1
Taeyang Park <101627885+gitubanana@users.noreply.github.com>
2024-09-05 23:38:44 +0900

    [Bronze V] Title: Rectangles, Time: 4 ms, Memory: 860 KB -BaekjoonHub

--- 백준/Bronze/15232. Rectangles/Rectangles.go
+++ 백준/Bronze/15232. Rectangles/Rectangles.go
@@ -2,16 +2,16 @@ package main
 
 import (
   "fmt"
+  "strings"
 )
 
 func main() {
   var ySize, xSize int
 
   fmt.Scan(&ySize, &xSize)
+
+  line := strings.Repeat("*", xSize)
   for y := 0; y < ySize; y++ {
-    for x := 0; x < xSize; x++ {
-      fmt.Print("*")
-    }
-    fmt.Println()
+    fmt.Println(line)
   }
 }
