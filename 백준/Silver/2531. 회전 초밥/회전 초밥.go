c4ad180f1df4c08c82f3b9056ab35d6e9289f317
Taeyang Park <ptyolo666@gmail.com>
2024-09-20 11:09:47 +0900

    [Silver I] Title: 회전 초밥, Time: 72 ms, Memory: 1264 KB -BaekjoonHub

---
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+)
+
+var (
+	reader = bufio.NewReader(os.Stdin)
+	writer = bufio.NewWriter(os.Stdout)
+)
+
+func getTypeCnt(tCnt []int) (typeCnt int) {
+	for t := 1; t < len(tCnt); t++ {
+		if tCnt[t] > 0 {
+			typeCnt++
+		}
+	}
+	return
+}
+
+func main() {
+	defer writer.Flush()
+
+	var arrSize, typeCnt, windowSize, coupon int
+
+	fmt.Fscan(reader, &arrSize, &typeCnt, &windowSize, &coupon)
+	arr := make([]int, arrSize)
+	for i := 0; i < arrSize; i++ {
+		fmt.Fscan(reader, &arr[i])
+	}
+
+	tCnt := make([]int, typeCnt + 1)
+	for i := 0; i < windowSize; i++ {
+		tCnt[arr[i]]++
+	}
+	tCnt[coupon]++
+
+	maxType := getTypeCnt(tCnt)
+	prev := 0
+	for {
+		cur := (prev + windowSize) % arrSize
+
+		tCnt[arr[prev]]--
+		tCnt[arr[cur]]++
+		maxType = max(maxType, getTypeCnt(tCnt))
+
+		prev = (prev + 1) % arrSize
+		if prev == 0 {
+			break
+		}
+	}
+
+	fmt.Fprintln(writer, maxType)
+}
