9577721dcc426dc0e5af597d673eee11cdd54936
Taeyang Park <ptyolo666@gmail.com>
2024-09-30 09:30:53 +0900

    [Bronze V] Title: Java Warriors, Time: 60 ms, Memory: 11516 KB -BaekjoonHub

---
+++ 백준/Bronze/30328. Java Warriors/Java Warriors.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.Inet4Address;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(Integer.parseInt(br.readLine()) * 4000);
+    }
+}
+
