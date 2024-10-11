64d8cb4601dc8790cf30121dcf5918f147cd3211
Taeyang Park <ptyolo666@gmail.com>
2024-10-11 10:30:29 +0900

    [Silver III] Title: 이항 쇼다운, Time: 60 ms, Memory: 11676 KB -BaekjoonHub

---
+++ 백준/Silver/6591. 이항 쇼다운/이항 쇼다운.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    static double comb(int n, int r) {
+        r = Math.min(r, n - r);
+
+        double permutation = 1;
+        double factorial = 1;
+
+        for (int i = 0; i < r; ++i) {
+            permutation *= (n - i);
+            factorial *= (i + 1);
+        }
+
+        return permutation / factorial;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int r = Integer.parseInt(st.nextToken());
+
+            if (n == 0) {
+                break;
+            }
+
+            bw.write((int)comb(n, r)+"\n");
+        }
+
+        bw.flush();
+    }
+}
