b21e161be79a25b0ebefc3c0d82441e6ab95692a
Taeyang Park <ptyolo666@gmail.com>
2024-10-22 10:35:26 +0900

    [Bronze V] Title: Just Round Down, Time: 68 ms, Memory: 11492 KB -BaekjoonHub

---
+++ 백준/Bronze/32458. Just Round Down/Just Round Down.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine(), ".");
+        System.out.println(st.nextToken());
+    }
+}
