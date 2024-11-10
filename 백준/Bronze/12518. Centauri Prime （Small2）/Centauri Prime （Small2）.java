541bc76731ce6a8123e05711d8b48dd171a62f15
Taeyang Park <ptyolo666@gmail.com>
2024-11-10 09:12:43 +0900

    [Bronze II] Title: Centauri Prime (Small2), Time: 100 ms, Memory: 13468 KB -BaekjoonHub

---
+++ 백준/Bronze/12518. Centauri Prime （Small2）/Centauri Prime （Small2）.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static final String VOWELS = "aeiouAEIOU";
+    static final String TURMOIL = "yY";
+
+    public static void main(String[] args) throws Exception {
+        int testCnt = Integer.parseInt(br.readLine());
+
+        for (int t = 1; t <= testCnt; t++) {
+            bw.append("Case #");
+            bw.append(t+": ");
+
+            String name = br.readLine();
+            bw.append(
+                String.format(
+                    "%s is ruled by %s.\n",
+                    name,
+                    findRuler(name)
+                )
+            );
+        }
+        bw.flush();
+    }
+
+    static String findRuler(String name) {
+        String last = name.substring(name.length() - 1);
+
+        if (TURMOIL.contains(last)) {
+            return "nobody";
+        }
+
+        if (VOWELS.contains(last)) {
+            return "a queen";
+        }
+
+        return "a king";
+    }
+}
