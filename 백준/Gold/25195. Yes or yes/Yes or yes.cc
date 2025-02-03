1c1491267c07f42f068006a74af92d843a8bcace
Taeyang Park <ptyolo666@gmail.com>
2025-02-04 08:07:32 +0900

    [Gold IV] Title: Yes or yes, Time: 44 ms, Memory: 9800 KB -BaekjoonHub

---
+++ 백준/Gold/25195. Yes or yes/Yes or yes.cc
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <vector>
+
+const int MAX_V = 1e5 + 1;
+const int START = 1;
+
+int vCnt;
+bool isFan[MAX_V];
+bool visited[MAX_V];
+std::vector<int> edges[MAX_V];
+
+bool canAvoidFans(int cur=START) {
+    visited[cur] = true;
+    if (isFan[cur]) {
+        return false;
+    }
+
+    for (const int &next : edges[cur]) {
+        if (visited[next]) {
+            continue;
+        }
+
+        if (canAvoidFans(next)) {
+            return true;
+        }
+    }
+    return edges[cur].empty();
+}
+
+int main(void) {
+    int eCnt;
+
+    fscanf(stdin, " %d %d", &vCnt, &eCnt);
+    while (eCnt-- > 0) {
+        int from, to;
+
+        fscanf(stdin, " %d %d", &from, &to);
+        edges[from].push_back(to);
+    }
+
+    int fanCnt;
+
+    fscanf(stdin, " %d", &fanCnt);
+    while (fanCnt-- > 0) {
+        int pos;
+
+        fscanf(stdin, " %d", &pos);
+        isFan[pos] = true;
+    }
+
+    const char *msg[] = {"Yes", "yes"};
+
+    fprintf(stdout, "%s\n", msg[canAvoidFans()]);
+    return 0;
+}
