be02489cf8db982a0ebf35431c8c878c843456da
Taeyang Park <ptyolo666@gmail.com>
2025-08-02 09:10:39 +0900

    [Bronze III] Title: Hurricane Warning, Time: 44 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/30266. Hurricane Warning/Hurricane Warning.cc
+++ 백준/Bronze/30266. Hurricane Warning/Hurricane Warning.cc
@@ -1,26 +1,24 @@
 #include <stdio.h>
-#include <string.h>
 
 int main(void) {
     int testCnt;
     char str[11];
-    bool isInformed[26];
 
     scanf(" %d", &testCnt);
     for (int t = 1; t <= testCnt; t++) {
         int peopleCnt;
+        int isInformed = 0;
 
         scanf(" %d %s", &peopleCnt, str);
-        memset(isInformed, false, sizeof(isInformed));
         for (int i = 0; str[i]; i++) {
-            isInformed[str[i] - 'A'] = true;
+            isInformed |= 1 << (str[i] - 'A');
         }
 
         int informedCnt = 0;
         for (int p = 0; p < peopleCnt; p++) {
             scanf(" %s", str);
             for (int i = 0; str[i]; i++) {
-                if (isInformed[str[i] - 'A']) {
+                if (isInformed & (1 << (str[i] - 'A'))) {
                     informedCnt++;
                     break;
                 }
